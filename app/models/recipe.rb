719c66d7fa22e408d85ee33d6bcc3e62d142e2ce
mattyo7126 <hyacinth.126@icloud.com>
2022-04-17 16:07:17 +0900

    ActiveHashの導入

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,14 @@
 class Recipe < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :time
+  belongs_to :category
+  belongs_to :quantity
+
+
+
+  with_options numericality: { other_than: 1 } do
+    validates :time_id
+    validates :category_id
+    validates :quantity_id
+  end
 end
