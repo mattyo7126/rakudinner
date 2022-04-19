5e8ce2db961695f21e3622adaab2eed73a016dc6
mattyo7126 <hyacinth.126@icloud.com>
2022-04-19 11:02:15 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "recipes#index"
-  resources :recipes, only: [:index, :new, :create, :show]
+  resources :recipes, only: [:index, :new, :create, :show, :edit, :update]
   resources :users, only: :show
 end
