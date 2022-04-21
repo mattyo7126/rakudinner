8dde13cff325b06b20d0668e1e8c636fb486d28d
mattyo7126 <hyacinth.126@icloud.com>
2022-04-21 13:09:18 +0900

    favorite テーブルの作成

--- app/javascript /preview.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const postForm = document.getElementById('new_post');
-  if (!postForm) return null;
-  console.log("preview.jsが読み込まれました");
-});
