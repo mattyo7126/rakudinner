44756005cc49bd6e1908f84378cd60e3879e5387
mattyo7126 <hyacinth.126@icloud.com>
2022-04-22 15:55:28 +0900

    材料から検索する実装

--- app/models/cooking_time.rb
+++ app/models/cooking_time.rb
@@ -1,12 +1,11 @@
 class CookingTime < ActiveHash::Base
   self.data = [
-    { id: 1, name: '--' },
-    { id: 2, name: '5分' },
-    { id: 3, name: '10分' },
-    { id: 4, name: '15分' },
-    { id: 5, name: '20分' },
-    { id: 6, name: '25分' },
-    { id: 7, name: '30分' }
+    { id: 1, name: '5分' },
+    { id: 2, name: '10分' },
+    { id: 3, name: '15分' },
+    { id: 4, name: '20分' },
+    { id: 5, name: '25分' },
+    { id: 6, name: '30分' }
   ]
 
   include ActiveHash::Associations
