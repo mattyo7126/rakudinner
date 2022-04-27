43fa7970c8d828d86c8c0173826ccdbcb9de2ad6
mattyo7126 <hyacinth.126@icloud.com>
2022-04-27 22:01:18 +0900

    不要なテーブルの削除

--- db/migrate/20220418001726_rename_time_id_column_to_recipes.rb
+++
@@ -1,5 +0,0 @@
-class RenameTimeIdColumnToRecipes < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :recipes, :time_id, :cooking_time_id
-  end
-end
--- db/migrate/20220424014015_change_data_cooking_history_id_to_users.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataCookingHistoryIdToUsers < ActiveRecord::Migration[6.0]
-  def change
-    change_column :users, :cooking_history_id, :text
-  end
-end
